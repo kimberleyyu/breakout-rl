bd8061ba5ea132de55b2902720ef16f526892ae8
lhz1029 <lilyhongzhang@college.harvard.edu>
2016-12-10 22:29:38 -0500

    started documenting game.py

--- game.py
+++ game.py
@@ -19,6 +19,7 @@ SCREEN_R = 152
 PADDLE_LEN = 16
 
 def ballFell(state):
+    """ determine from the image if the ball fell off the screen"""
     for i in range(SCREEN_L, SCREEN_R):
         if state[TOP_PADDLE_ROW][i][0] == 0 and state[TOP_PADDLE_ROW][i+1][0] != 0 and state[TOP_PADDLE_ROW][i+3][0] == 0:
             return True
@@ -42,42 +43,60 @@ alpha = args.alpha
 numTraining = args.numtrain
 qplus = args.qplus
 
-## run breakout
+# create the environment for Breakout
 env = gym.make('Breakout-v0')
+
 # for monitoring how we are doing
 # env.monitor.start('breakout-experiment-3')
 
+# get the legal actions as a list
 legalActions = range(env.action_space.n)
+# qplus is our agent which the additional features of where the ball will land relative to the paddle
+# qplus takes in the current and previous state to determine the best action
 if qplus:
     agent = qlearningagent.QLearnerPlus(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining)
+# else, we use only use our base features
 else:
     agent = qlearningagent.QLearner(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining)
+# lists to record the lengths of the episodes, total rewards, and weights for graphing
 lengths = []
 rewards = []
 weights_over_time = []
+# extra precaution in place to prevent crashes during instances where weights blow up
 end = False
-# play the game 5 times
+
+# play the game 200 times
 for i_episode in range(200):
+    # keep track of the total reward for a game
     r = 0
+    # reset the environment for the beginninng of each game
     state = env.reset()
     if qplus:
         prev_state = state
+    # iterate through each timestep t in a game
     for t in range(10000):
+        # render the environment at that timestep
         env.render()
+        # get an action
         if qplus:
             action = agent.getAction(state, prev_state)
         else:
             action = agent.getAction(state)
+        # take the action and get the reward and nextState from the environment
         nextState, reward, done, info = env.step(action)
+        # update the total reward count
         r +=reward
+        # include a negative reward if the ball falls
         if ballFell(nextState):
             reward = -1
-
+        # update the weights
         if qplus:
             weights = agent.update(state, prev_state, action, nextState, reward)
         else:
             weights = agent.update(state, action, nextState, reward)
+        # keep track of the weights over time
         weights_over_time.append([weights['paddlex'],weights['ballx'], weights['bally']])
+        
         if abs(weights["ballx"]) > 10**305:
             print("Episode finished after {} timesteps with {} reward".format(t+1, r))
             lengths.append(t+1)
