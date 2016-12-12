9ef95c4e652cb0a882ed9a4b87e36c524a9205ae
Kimberley Yu <kimberleyyu@college.harvard.edu>
2016-12-12 00:43:32 -0500

    Q lambda

--- game.py
+++ game.py
@@ -8,14 +8,18 @@
 # use --epsilon, --gamma, --alpha, and/or --numtrain if you want to adjust those parameters. The first 3 must be floats and the last must be an int.
 
 import gym
-import qlearningagent
+import qlearningagentlambda
 import numpy as np
 import matplotlib.pyplot as plt
 import argparse
 
+BOTTOM_BLOCK_ROW = 93
 TOP_PADDLE_ROW = 189
 SCREEN_L = 8
 SCREEN_R = 152
+MIDDLE_X = (SCREEN_L+SCREEN_R)/2
+MIDDLE_Y = (BOTTOM_BLOCK_ROW+TOP_PADDLE_ROW)/2
+
 PADDLE_LEN = 16
 
 def ballFell(state):
@@ -25,6 +29,25 @@ def ballFell(state):
             return True
     return False
 
+def ballHit(state):
+    """ determine from the image if the paddle hit the ball (x positions match)"""
+    paddle_xpos = state[TOP_PADDLE_ROW, SCREEN_L:SCREEN_R, 0]
+    paddlex = next((i for i, x in enumerate(paddle_xpos) if x), MIDDLE_X)
+
+    ball_xpos = np.sum(state[BOTTOM_BLOCK_ROW:TOP_PADDLE_ROW, SCREEN_L:SCREEN_R, 0], axis=0)
+    ballx = next((i for i, x in enumerate(ball_xpos) if x), MIDDLE_X)
+
+    ball_ypos = state[BOTTOM_BLOCK_ROW:TOP_PADDLE_ROW, ballx, 0]
+    bally = next((i for i, x in enumerate(ball_ypos) if x), MIDDLE_Y)
+    #print ballx
+    #features["paddlex"] = features["paddlex"]/32
+    #features["ballx"] = features["ballx"]/32
+
+    #if ballx - paddlex < PADDLE_LEN and ballx - paddlex > 0:
+    if abs(ballx - paddlex) < 9 and abs(bally - TOP_PADDLE_ROW) < 10 :
+        return True
+    return False
+
 ## input command-line arguments
 parser = argparse.ArgumentParser()
 parser.add_argument('--epsilon', dest='epsilon', default=0.05, required=False, type=float, help='exploration rate for epsilon-greedy')
@@ -32,8 +55,8 @@ parser.add_argument('--gamma', default=0.99, required=False, type=float, help='d
 parser.add_argument('--alpha', default=0.02, required=False, type=float, help='learning rate')
 parser.add_argument('--numtrain', default=10000, required=False, type=int, help='number of training episodes')
 
-parser.add_argument('--QLearnerPlus', dest='qplus', action='store_true', help="Add this argument if you want to run QLearnerPlus.")
-parser.set_defaults(feature=False).
+parser.add_argument('--lambd', default=0.9, required=False, type=int, help='decay rate of eligibility traces')
+parser.add_argument('--version', default=1, required=False, type=int, help='1 for QLearner; 2 for QLearnerPlus; 3 for QLearnerPlusLambda')
 
 args = parser.parse_args()
 
@@ -41,7 +64,8 @@ epsilon = args.epsilon
 gamma = args.gamma
 alpha = args.alpha
 numTraining = args.numtrain
-qplus = args.qplus
+lambd = args.lambd
+version = args.version
 
 # create the environment for Breakout
 env = gym.make('Breakout-v0')
@@ -53,11 +77,17 @@ env = gym.make('Breakout-v0')
 legalActions = range(env.action_space.n)
 # qplus is our agent which the additional features of where the ball will land relative to the paddle
 # qplus takes in the current and previous state to determine the best action
-if qplus:
-    agent = qlearningagent.QLearnerPlus(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining)
-# else, we use only use our base features
+if version==1:
+    agent = qlearningagentlambda.QLearner(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining)
+elif version==2:
+    agent = qlearningagentlambda.QLearnerPlus(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining)
+elif version==3:
+    agent = qlearningagentlambda.QLearnerPlusLambda(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining, lambd=lambd)
 else:
-    agent = qlearningagent.QLearner(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining)
+    print "NOT A VALID VERSION. PLEASE CHOOSE 1, 2, OR 3. Defaulting to version 1."
+    version = 1
+    agent = qlearningagentlambda.QLearner(legalActions, epsilon=epsilon,gamma=gamma,alpha=alpha, numTraining=numTraining)
+
 # lists to record the lengths of the episodes, total rewards, and weights for graphing
 lengths = []
 rewards = []
@@ -69,44 +99,56 @@ end = False
 for i_episode in range(200):
     # keep track of the total reward for a game
     r = 0
-    # reset the environment for the beginninng of each game
+    # reset the environment for the beginning of each game
     state = env.reset()
-    if qplus:
+    if version>1:
         prev_state = state
+        action = 0 #env.action_space[0] # initialize a as first action
     # iterate through each timestep t in a game
     for t in range(10000):
         # render the environment at that timestep
         env.render()
         # get an action
-        if qplus:
-            action = agent.getAction(state, prev_state)
-        else:
+        if version==1:
             action = agent.getAction(state)
+        elif version==2:
+            action = agent.getAction(state, prev_state)
         # take the action and get the reward and nextState from the environment
         nextState, reward, done, info = env.step(action)
+
         # update the total reward count
         r +=reward
         # include a negative reward if the ball falls
         if ballFell(nextState):
             reward = -1
+        # include a positive reward if ball hit
+        if ballHit(nextState):
+            reward += 1
+
         # update the weights
-        if qplus:
-            weights = agent.update(state, prev_state, action, nextState, reward)
-        else:
+        if version==1:
             weights = agent.update(state, action, nextState, reward)
+        elif version==2:
+            weights = agent.update(state, prev_state, action, nextState, reward)
+        elif version==3:
+            nextAction = agent.getAction(nextState, state)
+            weights = agent.update(state, prev_state, nextState, action, nextAction, reward)
+
         # keep track of the weights over time
         weights_over_time.append([weights['paddlex'],weights['ballx'], weights['bally']])
-        
+
         if abs(weights["ballx"]) > 10**305:
             print("Episode finished after {} timesteps with {} reward".format(t+1, r))
             lengths.append(t+1)
             rewards.append(r)
             end = True
             break
-        if qplus:
-            nextState = state
+
+        if version > 1:
             prev_state = state
-            state = nextState
+            if version==3:
+                action = nextAction
+        state = nextState
         # end if done
         if done:
             print("Episode finished after {} timesteps with {} reward".format(t+1, r))
--- qlearningagent.py
+++ qlearningagent.py
@@ -1,19 +1,33 @@
 ### Frances Ding, Lily Zhang, Kimberley Yu
 
+# q learning with eligibility traces
+# http://webdocs.cs.ualberta.ca/~sutton/book/ebook/node78.html
+# http://stackoverflow.com/questions/40862578/how-to-understand-watkinss-q%CE%BB-learning-algorithm-in-suttonbartos-rl-book/40899753#40899753
+
 import featureExtractor, util
 import random
+import itertools
 
+
+# Basic Q learner with simple features:
+# x and y position of ball + x position of paddle
 class QLearner:
     def __init__(self, legalActions, epsilon=0.05,gamma=0.99,alpha=0.2, numTraining=1000):
+        """
+            Initializes the QLearner.
+        """
         self.legalActions = legalActions
         self.epsilon = epsilon
         self.gamma = gamma
         self.alpha = alpha
         self.numTraining = numTraining
         self.weights = util.Counter()
-        self.discount = 0.8
 
     def getQValue(self, state, action):
+        """
+            Calculates and returns the Q-value for the (state, action) pair
+            using the feature extractor. (Approximate Q-Learning).
+        """
         features = featureExtractor.getFeatures(state, action) # THIS DEPENDS ON FEATURE EXTRACTOR
         feature_keys = features.keys()
         feature_keys.sort()
@@ -50,6 +64,10 @@ class QLearner:
         return action
 
     def update(self, state, action, nextState, reward):
+        """
+            Performs the Q-learning update function and returns the new weights
+            for all features.
+        """
         # extract features
         features = featureExtractor.getFeatures(state, action) #THIS DEPENDS ON FEATURE EXTRACTOR INTERFACE
         feature_keys = features.keys()
@@ -65,7 +83,7 @@ class QLearner:
         else:
             max_value = 0.0
         # calculate difference between predicted and observed value
-        difference = (reward + self.discount*max_value) - self.getQValue(state,action)
+        difference = (reward + self.gamma*max_value) - self.getQValue(state,action)
         # loop over features to update their weights
         for feature in feature_keys:
             self.weights[feature] = self.weights[feature] + self.alpha*difference*features[feature]
@@ -74,9 +92,16 @@ class QLearner:
             self.epsilon = 0
         return self.weights
 
+# Inherits from QLearner class.
+# Added features:
+# Predicted landing x position of ball + relative position of ball to paddle
 class QLearnerPlus(QLearner):
 
     def getQValue(self, state, prev_state, action):
+        """
+            Calculates and returns the Q-value for the (state, action) pair
+            using the feature extractor. (Approximate Q-Learning).
+        """
         features = featureExtractor.getFeaturesPlus(state, prev_state, action) # THIS DEPENDS ON FEATURE EXTRACTOR
         feature_keys = features.keys()
         feature_keys.sort()
@@ -113,6 +138,10 @@ class QLearnerPlus(QLearner):
         return action
 
     def update(self, state, prev_state, action, nextState, reward):
+        """
+            Performs the Q-learning update function and returns the new weights
+            for all features.
+        """
         # extract features
         features = featureExtractor.getFeaturesPlus(state, prev_state, action) #THIS DEPENDS ON FEATURE EXTRACTOR INTERFACE
         feature_keys = features.keys()
@@ -128,11 +157,113 @@ class QLearnerPlus(QLearner):
         else:
             max_value = 0.0
         # calculate difference between predicted and observed value
-        difference = (reward + self.discount*max_value) - self.getQValue(state,prev_state, action)
+        difference = (reward + self.gamma*max_value) - self.getQValue(state,prev_state, action)
         # loop over features to update their weights
         for feature in feature_keys:
             self.weights[feature] = self.weights[feature] + self.alpha*difference*features[feature]
         self.numTraining -= 1
         if self.numTraining < 0:
             self.epsilon = 0
+        return self.weights
+
+# Inherits from QLearnerPlus class.
+# Includes eligibility traces
+# lambda parameter controls decay of eligibility trace
+class QLearnerPlusLambda(QLearnerPlus):
+    # def __init__(self, lambd=0.9):
+    #     """
+    #         Initializes the QLearner with additional lambda parameter.
+    #     """
+    #     super(QLearnerPlusLambda, self).__init__()
+    #     self.lambd = lambd
+    #     self.eligibility = util.Counter()
+
+    def __init__(self, legalActions, epsilon=0.05,gamma=0.99,alpha=0.2, numTraining=1000, lambd=0.9):
+        """
+            Initializes the QLearner.
+        """
+        self.legalActions = legalActions
+        self.epsilon = epsilon
+        self.gamma = gamma
+        self.alpha = alpha
+        self.numTraining = numTraining
+        self.weights = util.Counter()
+
+        self.lambd = lambd
+        self.eligibility = util.Counter()
+
+    def getQValue(self, state, prev_state, action):
+        """
+            Calculates and returns the Q-value for the (state, action) pair
+            using the feature extractor. (Approximate Q-Learning).
+        """
+        features = featureExtractor.getFeaturesPlus(state, prev_state, action) # THIS DEPENDS ON FEATURE EXTRACTOR
+        feature_keys = features.keys()
+        feature_keys.sort()
+        q_sum = 0
+        for feature in feature_keys:
+            q_sum = q_sum + self.weights[feature]*features[feature] #increment q value sum
+        return q_sum
+
+    def computeActionFromQValues(self, state, prev_state):
+        """
+          Computes the best action to take in a state.  If no legal actions,
+          eg. at the terminal state, returns None.
+        """
+        actions = self.legalActions
+        vals = [self.getQValue(state, prev_state, a) for a in actions]
+        maxVal = max(vals)
+        bestActions = [a for a in actions if self.getQValue(state, prev_state, a) == maxVal]
+        return random.choice(bestActions)
+
+    def getAction(self, state, prev_state):
+        """
+          Computes the action to take in the current state.  With
+          probability self.epsilon, it takes a random action and
+          take the best policy action otherwise.  If there are
+          no legal actions, eg. at the terminal state, returns None.
+        """
+        # Pick Action
+        actions = self.legalActions
+        action = None
+        if util.flipCoin(self.epsilon):
+            return random.choice(actions)
+        else:
+            action = self.computeActionFromQValues(state, prev_state)
+        return action
+
+    def update(self, state, prev_state, nextState, action, nextAction, reward):
+        """
+            Performs the Q-learning update function and returns the new weights
+            for all features.
+        """
+
+        astar = self.computeActionFromQValues(nextState, state)
+        delta = reward + self.gamma*self.getQValue(nextState, state, astar) - self.getQValue(state, prev_state, action)
+
+        # extract features
+        features = featureExtractor.getFeaturesPlus(state, prev_state, action) #THIS DEPENDS ON FEATURE EXTRACTOR INTERFACE
+        feature_keys = features.keys()
+        feature_keys.sort()
+        for feature in feature_keys:
+            # have an eligibility trace for each feature?
+            self.eligibility[feature] = self.eligibility[feature] + 1
+
+        # for all features
+        ## wait lols don't think this is necessary
+        # features_space = itertools.product(range(8, 152)/32, range(93,189)/50, range(8, 152)/32, range(8, 152)/32)
+        # for feat_values in features_space:
+        #     feat_dict = dict(zip(feature_keys, feat_values))
+
+        for feature in feature_keys:
+            self.weights[feature] = self.weights[feature] + self.alpha * delta * self.eligibility[feature]
+            if nextAction == astar:
+                self.eligibility[feature] = self.gamma * self.lambd * self.eligibility[feature]
+            else:
+                self.eligibility[feature] = 0
+
+        self.numTraining -= 1
+        if self.numTraining < 0:
+            self.epsilon = 0
+
         return self.weights
